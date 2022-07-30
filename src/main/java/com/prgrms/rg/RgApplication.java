a8e888f6c9c34127eb7b419f7ff12411ca6fe190
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-07-30 19:34:28 +0900

    [refactoring] 오타 수정, 더미파일 삭제

--- src/main/java/com/prgrms/rg/RgApplication.java
+++ src/main/java/com/prgrms/rg/RgApplication.java
@@ -2,9 +2,8 @@ package com.prgrms.rg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class RgApplication {
 	public static void main(String[] args) {
 		// 애플리케이션을 ec2에서만 사용할 것이 아니므로, spring cloud aws에서 ec2메타데이터를 받아오는 것이 불필요함
--- src/main/java/com/prgrms/rg/domain/ridingpost/application/dummy.java
+++
@@ -1,4 +0,0 @@
-package com.prgrms.rg.domain.ridingpost.application;
-
-public class dummy {
-}
