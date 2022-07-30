27e0d59b1fe26e7e513ec6f5d9a4d6975f53db39
Minsung Kim <epicblue@hanmail.net>
2022-07-30 12:30:43 +0900

    [chore] FileStoreSpy @Primary 적용
    테스트환경에서는 실제 FileStore 구현체를 빈으로 등록시키지 않기 위함

--- src/test/java/com/prgrms/rg/AppTest.java
+++ src/test/java/com/prgrms/rg/AppTest.java
@@ -2,13 +2,9 @@ package com.prgrms.rg;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import java.sql.SQLException;
-
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,10 +14,8 @@ class AppTest {
 	@Autowired
 	DataSource dataSource;
 
-	Logger log = LoggerFactory.getLogger(getClass().getSimpleName());
-
 	@Test
-	void check_database_connection() throws SQLException {
-		assertThat(dataSource.getConnection()).isNotNull();
+	void check_database_connection() {
+		assertThatNoException().isThrownBy(() -> dataSource.getConnection());
 	}
 }
