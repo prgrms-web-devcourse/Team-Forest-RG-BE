8976b2a4c858e5209f4683e11af4fb403d3f09be
Minsung Kim <epicblue@hanmail.net>
2022-07-30 09:27:32 +0900

    [refactor] SpyFileStore 검증 명령 캐싱 식별자 변경
    검증 명령 식별자 (파일 이름 + multipartFile 문자열 직렬화 값)

--- src/test/java/com/prgrms/rg/infrastructure/file/SpyFileStore.java
+++ src/test/java/com/prgrms/rg/infrastructure/file/SpyFileStore.java
@@ -13,14 +13,14 @@ import com.prgrms.rg.domain.common.file.FileStore;
 
 /**
  * FileStore 테스트용 spy 클래스
- * FileStore를 검증하고 싶을 경우 @Autowired를 할 때 SpyFileStore를 선언해야 아래의 검증 메서드를 사용할 수 있습니다.
- * 검증 사용이 불편하면 이 파일을 무시하시고 일반적인 스프링 테스트처럼 @MockBean을 사용하셔도 됩니다.
+ * 테스트 환경에서 운영용 FileStore 구현체 대신 DI 됩니다.
+ * 동작을 검증하고 싶을 경우 @Autowired를 할 때 SpyFileStore 타입으로 선언해야 아래의 검증 메서드를 사용할 수 있습니다.
+ * 검증 사용이 불편하면 이 파일을 무시하시고 일반적인 스프링 테스트처럼 @MockBean을 통해 Mockito 방식으로 FileStore를 테스트 하시면 됩니다.
  */
 @Component
 public class SpyFileStore implements FileStore {
 
 	String prefix = "https://rg.storage.url/";
-
 
 	Map<String, Integer> saveCommandCache = new HashMap<>();
 	Map<String, Integer> deleteCommandCache = new HashMap<>();
@@ -28,8 +28,8 @@ public class SpyFileStore implements FileStore {
 	/*
 	 * 이미지 전송 요청이 제대로 전달 되었는가 확인하는 메서드
 	 */
-	public void assertSaveCommandCalledOnce(String url) {
-		assertThat(saveCommandCache).containsEntry(url, 1);
+	public void assertSaveCommandCalledOnce(MultipartFile savedFile, String savedFileName) {
+		assertThat(saveCommandCache).containsEntry(hashToCommandKey(savedFileName, savedFile), 1);
 	}
 
 	/*
@@ -43,20 +43,17 @@ public class SpyFileStore implements FileStore {
 	public String save(MultipartFile multipartFile, String fileName) throws FileIOException {
 		assertThat(multipartFile).isNotNull();
 		var url = prefix + fileName;
-		if(saveCommandCache.containsKey(url)) {
-			saveCommandCache.put(url, saveCommandCache.get(url) + 1);
-			return url;
-		}
-		saveCommandCache.put(url, 1);
+		saveCommandCache.put(hashToCommandKey(fileName, multipartFile), saveCommandCache.getOrDefault(url, 0) + 1);
 		return url;
 	}
 
 	@Override
 	public void delete(String fileUrl) {
-		if(deleteCommandCache.containsKey(fileUrl)) {
-			deleteCommandCache.put(fileUrl, deleteCommandCache.get(fileUrl) + 1);
-			return;
-		}
-		deleteCommandCache.put(fileUrl, 1);
+		deleteCommandCache.put(fileUrl, deleteCommandCache.getOrDefault(fileUrl, 0) + 1);
 	}
+
+	private String hashToCommandKey(String fileName, MultipartFile multipartFile) {
+		return fileName + " " + multipartFile.toString();
+	}
+
 }
