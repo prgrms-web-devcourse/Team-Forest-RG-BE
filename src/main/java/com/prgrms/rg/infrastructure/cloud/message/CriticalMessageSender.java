8b615a51604c53a99aaa3b9b6ce8fe43c0e58b23
Minsung Kim <epicblue@hanmail.net>
2022-08-02 02:09:27 +0900

    [chore] CI/CD 환경 설정

--- src/main/java/com/prgrms/rg/infrastructure/cloud/message/CriticalMessageSender.java
+++ src/main/java/com/prgrms/rg/infrastructure/cloud/message/CriticalMessageSender.java
@@ -1,8 +1,10 @@
 package com.prgrms.rg.infrastructure.cloud.message;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -22,8 +24,16 @@ public class CriticalMessageSender {
 
 	public void send(Exception exception) throws JsonProcessingException {
 		ObjectMapper json = new ObjectMapper();
-		String body = json.writeValueAsString(Map.of("channel", SLACK_CHANNEL_ID, "text", exception.fillInStackTrace()
-			.getMessage()));
+		var exceptionStream = new ByteArrayOutputStream();
+		exception.printStackTrace(new PrintStream(exceptionStream));
+		String emoji = ":meow_sad:\t";
+		StringBuilder emojiBuffer = new StringBuilder();
+		emojiBuffer.append(emoji.repeat(20));
+		emojiBuffer.append("\n").append(exceptionStream.toString(StandardCharsets.UTF_8)).append("\n");
+		emojiBuffer.append(emoji.repeat(20));
+
+		String body = json.writeValueAsString(
+			Map.of("channel", SLACK_CHANNEL_ID, "text", emojiBuffer));
 		try {
 			URL url = new URL("https://slack.com/api/chat.postMessage");
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
@@ -31,7 +41,7 @@ public class CriticalMessageSender {
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Content-Type", "application/json");
 			conn.setRequestProperty("Accept-Charset", "UTF-8");
-			conn.setConnectTimeout(5000); // 커넥션 생성 대기 시간(해당 시간 안에 커넥션 생성 안되면 SocketTimeoutException)
+			conn.setConnectTimeout(5000); // 커넥션 생성 대기 시간(해당 시간 안에 커넥션 생성 안되면 SocketTimeoutException
 			conn.setReadTimeout(5000);
 			conn.setRequestProperty("Authorization",
 				"Bearer " + SLACK_AUTH_TOKEN);
