f3e9b89fa44df3cbc594f0f814a1b7edbc93e5d8
Minsung Kim <epicblue@hanmail.net>
2022-08-01 21:37:24 +0900

    [feat] 슬랙 오류 메시지 전송 모듈

--- src/main/java/com/prgrms/rg/RgApplication.java
+++ src/main/java/com/prgrms/rg/RgApplication.java
@@ -1,16 +1,33 @@
 package com.prgrms.rg;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.prgrms.rg.infrastructure.cloud.message.CriticalMessageSender;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class RgApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// 애플리케이션을 ec2에서만 사용할 것이 아니므로, spring cloud aws에서 ec2메타데이터를 받아오는 것이 불필요함
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-		SpringApplication.run(RgApplication.class, args);
+
+		/*
+		 * 초기 profile entry point를 확인합니다. (prod 아니면 default)
+		 */
+		var initialProfile = System.getProperty("spring.profiles.active");
+		try {
+			SpringApplication.run(RgApplication.class, args);
+		} catch (Exception exception) {
+			// 초기 profile이 prod를 포함할 경우에만 특정 모듈에 메시지를 보냅니다.
+			if (initialProfile != null && initialProfile.matches("prod")) {
+				new CriticalMessageSender().send(exception);
+			}
+			throw exception;
+		}
 
 	}
 }
---
+++ src/main/java/com/prgrms/rg/infrastructure/cloud/message/CriticalMessageSender.java
@@ -0,0 +1,53 @@
+package com.prgrms.rg.infrastructure.cloud.message;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 스프링에서 처리하지 못한 예외 메시지를 자바 애플리케이션 종료 직전에 특정 컴포넌트에 전송시킵니다.
+ * TODO : 추상화
+ */
+public class CriticalMessageSender {
+
+	private static final String SLACK_CHANNEL_ID = System.getenv("SLACK_CHANNEL_NAME");
+	private static final String SLACK_AUTH_TOKEN = System.getenv("SLACK_AUTH_TOKEN");
+
+	public void send(Exception exception) throws JsonProcessingException {
+		ObjectMapper json = new ObjectMapper();
+		String body = json.writeValueAsString(Map.of("channel", SLACK_CHANNEL_ID, "text", exception.fillInStackTrace()
+			.getMessage()));
+		try {
+			URL url = new URL("https://slack.com/api/chat.postMessage");
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept-Charset", "UTF-8");
+			conn.setConnectTimeout(5000); // 커넥션 생성 대기 시간(해당 시간 안에 커넥션 생성 안되면 SocketTimeoutException)
+			conn.setReadTimeout(5000);
+			conn.setRequestProperty("Authorization",
+				"Bearer " + SLACK_AUTH_TOKEN);
+
+			OutputStream os = conn.getOutputStream();
+			os.write(body.getBytes(StandardCharsets.UTF_8));
+			os.flush();
+
+			// 리턴된 결과 읽기
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			in.lines().forEach((str) -> {
+			});
+			conn.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
