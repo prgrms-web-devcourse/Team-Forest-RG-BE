37c7f28f527cbae9d06eee7bb936e8393a03a97f
Hanbin Lee <logos1018@gmail.com>
2022-08-02 17:10:50 +0900

    [rename] 메서드명 오타 수정

--- src/main/java/com/prgrms/rg/web/user/api/UserRestControllerAdvice.java
+++ src/main/java/com/prgrms/rg/web/user/api/UserRestControllerAdvice.java
@@ -1,8 +1,5 @@
 package com.prgrms.rg.web.user.api;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,7 +15,7 @@ public class UserRestControllerAdvice {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler
-	public String handleIllegalVoucherTypeException(NoSuchUserException e) {
+	public String handleNoSuchUserException(NoSuchUserException e) {
 		log.info(e.getMessage(), e);
 		return "유저 정보를 불러오는 데 실패했습니다. 적합한 id로 요청을 보내지 않았을 수 있으며, 요청을 보내는 사이에 해당 유저 정보가 삭제되었을 수 있습니다.";
 	}
