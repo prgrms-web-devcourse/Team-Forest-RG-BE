3a8d7b5d3a95738716197785d2236be0f661ab76
Minsung Kim <epicblue@hanmail.net>
2022-08-02 15:00:58 +0900

    [refactor] 메시지 전송 클래스 예외 처리 -> 던지기 인스턴스 -> 유틸리티 static 클래스

--- src/main/java/com/prgrms/rg/RgApplication.java
+++ src/main/java/com/prgrms/rg/RgApplication.java
@@ -1,7 +1,5 @@
 package com.prgrms.rg;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,7 +9,7 @@ import com.prgrms.rg.infrastructure.cloud.CriticalMessageSender;
 @EnableJpaAuditing
 @SpringBootApplication
 public class RgApplication {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// 애플리케이션을 ec2에서만 사용할 것이 아니므로, spring cloud aws에서 ec2메타데이터를 받아오는 것이 불필요함
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 
@@ -24,7 +22,7 @@ public class RgApplication {
 		} catch (Exception exception) {
 			// 초기 profile이 prod를 포함할 경우에만 특정 모듈에 메시지를 보냅니다.
 			if (initialProfile != null && initialProfile.matches("prod")) {
-				new CriticalMessageSender().send(exception);
+				CriticalMessageSender.send(exception);
 			}
 			throw exception;
 		}
--- src/main/java/com/prgrms/rg/infrastructure/cloud/CriticalMessageSender.java
+++ src/main/java/com/prgrms/rg/infrastructure/cloud/CriticalMessageSender.java
@@ -11,48 +11,49 @@ import java.util.Map;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * 스프링에서 처리하지 못한 예외 메시지를 자바 애플리케이션 종료 직전에 외부 시스템에 전송합니다.
  * TODO : 추상화
  */
+@UtilityClass
 public class CriticalMessageSender {
 
 	private static final String SLACK_CHANNEL_ID = System.getenv("SLACK_CHANNEL_NAME");
 	private static final String SLACK_AUTH_TOKEN = System.getenv("SLACK_AUTH_TOKEN");
 
-	public void send(Exception exception) throws JsonProcessingException {
+	public static void send(Exception exception) throws Exception {
 		String body = createMessageBodyFrom(exception);
 		sendHttpRequest(body);
 	}
 
-	private void sendHttpRequest(String body) {
-		try {
-			URL url = new URL("https://slack.com/api/chat.postMessage");
-			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-			// request 헤더, 토큰 설정
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-			conn.setRequestProperty("Accept-Charset", "UTF-8");
-			conn.setConnectTimeout(5000); // 커넥션 생성 대기 시간(해당 시간 안에 커넥션 생성 안되면 SocketTimeoutException
-			conn.setReadTimeout(5000);
-			conn.setRequestProperty("Authorization", "Bearer " + SLACK_AUTH_TOKEN);
+	private static void sendHttpRequest(String body) throws Exception {
 
-			// Json을 Http 요청 body에 담는다.
-			OutputStream os = conn.getOutputStream();
-			os.write(body.getBytes(StandardCharsets.UTF_8));
-			os.flush();
+		URL url = new URL("https://slack.com/api/chat.postMessage");
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		// request 헤더, 토큰 설정
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Accept-Charset", "UTF-8");
+		conn.setConnectTimeout(5000); // 커넥션 생성 대기 시간(해당 시간 안에 커넥션 생성 안되면 SocketTimeoutException
+		conn.setReadTimeout(5000);
+		conn.setRequestProperty("Authorization", "Bearer " + SLACK_AUTH_TOKEN);
 
-			// Http 요청을 보내고 응답을 inputStream에 받아 온다.
-			conn.getInputStream();
+		// Json을 Http 요청 body에 담는다.
+		OutputStream os = conn.getOutputStream();
+		os.write(body.getBytes(StandardCharsets.UTF_8));
+		os.flush();
 
-			conn.disconnect();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// Http 요청을 보내고 응답을 inputStream에 받아 온다.
+		// 이 메서드를 사용하지 않으면 http 요청을 보내지 않기 때문에, 무조건 사용
+		conn.getInputStream();
+
+		conn.disconnect();
 	}
 
-	private String createMessageBodyFrom(Exception exception) throws JsonProcessingException {
+	private static String createMessageBodyFrom(Exception exception) throws JsonProcessingException {
 		ObjectMapper json = new ObjectMapper();
 		var stackStream = new ByteArrayOutputStream();
 		exception.printStackTrace(new PrintStream(stackStream));
