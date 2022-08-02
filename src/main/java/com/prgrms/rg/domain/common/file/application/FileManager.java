94dded276d420f49049be147f1080ea6d2176885
Hanbin Lee <logos1018@gmail.com>
2022-08-02 13:24:54 +0900

    [refactor] null을 그대로 반환하지 않게끔 변경

--- src/main/java/com/prgrms/rg/domain/common/file/application/FileManager.java
+++ src/main/java/com/prgrms/rg/domain/common/file/application/FileManager.java
@@ -1,6 +1,7 @@
 package com.prgrms.rg.domain.common.file.application;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,7 +11,7 @@ import com.prgrms.rg.domain.common.file.model.StoredFile;
 public interface FileManager {
 	<T extends ImageAttachable> List<StoredFile> store(List<MultipartFile> multipartFiles, T attached);
 
-	<T extends ImageAttachable> StoredFile store(MultipartFile multipartFile, T attached);
+	<T extends ImageAttachable> Optional<StoredFile> store(MultipartFile multipartFile, T attached);
 
 	<T1 extends StoredFile, T2 extends ImageAttachable> void delete(List<T1> files, T2 attached);
 
