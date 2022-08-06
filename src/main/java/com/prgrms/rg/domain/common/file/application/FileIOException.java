372114c9a3471334d210a60688e4bc4edc5a72ee
Hanbin Lee <logos1018@gmail.com>
2022-08-07 06:10:11 +0900

    [remove] 파일 레거시 코드 삭제

--- src/main/java/com/prgrms/rg/domain/common/file/application/FileIOException.java
+++
@@ -1,7 +0,0 @@
-package com.prgrms.rg.domain.common.file.application;
-
-public class FileIOException extends RuntimeException {
-	public FileIOException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
