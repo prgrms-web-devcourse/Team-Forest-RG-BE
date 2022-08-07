be3c98907103c1834b6f1d0d7d60a6e0d860b20b
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-08-07 16:27:16 +0900

    [fix] RidingPostNotFoundException 예외 메세지 수정

--- src/main/java/com/prgrms/rg/domain/ridingpost/model/RidingPostNotFoundException.java
+++ src/main/java/com/prgrms/rg/domain/ridingpost/model/RidingPostNotFoundException.java
@@ -2,7 +2,7 @@ package com.prgrms.rg.domain.ridingpost.model;
 
 public class RidingPostNotFoundException extends RuntimeException {
 	public RidingPostNotFoundException(Long id) {
-		super("cannot find RidingPost id: '" + id + "'");
+		super("유효하지 않은 게시글 id : '" + id + "'");
 	}
 
 }
