272d4bec65f827768bd63e553aead574fe37de67
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-08-07 16:43:32 +0900

    [feat] 라이딩 게시글 조회 컨트롤러 구현

---
+++ src/main/java/com/prgrms/rg/web/ridingpost/api/RidingInquiryExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.prgrms.rg.web.ridingpost.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.prgrms.rg.domain.ridingpost.model.RidingPostNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class RidingInquiryExceptionHandler {
+
+	@ExceptionHandler(RidingPostNotFoundException.class)
+	public ResponseEntity<String> handleRidingPostNotFoundException(RidingPostNotFoundException e) {
+		log.info(e.getMessage(), e);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+	}
+}
