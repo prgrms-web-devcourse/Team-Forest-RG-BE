51d9c4f63eaacc50accc70760e0785b887d7822c
kkj0419 <kkhi0419@gmail.com>
2022-08-07 22:29:47 +0900

    [add] riding 생성 예외처리 추가

--- src/main/java/com/prgrms/rg/web/ridingpost/api/RestRidingPostCreateController.java
+++ src/main/java/com/prgrms/rg/web/ridingpost/api/RestRidingPostCreateController.java
@@ -2,6 +2,7 @@ package com.prgrms.rg.web.ridingpost.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,9 @@ import com.prgrms.rg.domain.ridingpost.application.RidingPostService;
 import com.prgrms.rg.web.ridingpost.requests.RidingPostCreateRequest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController(value = "/api/v1")
 public class RestRidingPostCreateController {
@@ -19,9 +22,17 @@ public class RestRidingPostCreateController {
 	private final RidingPostService ridingPostService;
 
 	@PostMapping(value = "/ridingposts")
-	public ResponseEntity<Long> createRidingPost(@AuthenticationPrincipal JwtAuthentication auth, @RequestBody RidingPostCreateRequest ridingRequest) {
+	public ResponseEntity<Long> createRidingPost(@AuthenticationPrincipal JwtAuthentication auth,
+		@RequestBody RidingPostCreateRequest ridingRequest) {
 
 		//return type : post id
 		return ResponseEntity.ok(ridingPostService.createRidingPost(auth.userId, ridingRequest.toCommand()));
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handlerIllegalArgument(IllegalArgumentException e) {
+
+		log.warn("ILLEGAL ARGUMENT", e);
+		return ResponseEntity.badRequest().body(e.getMessage());
 	}
 }
