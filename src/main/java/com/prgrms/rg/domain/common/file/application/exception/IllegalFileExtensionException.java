158fc4fd65d2bf999ebf6329c5c175ebac46f76b
HunkiKim <gnsrl76@naver.com>
2022-08-07 21:46:22 +0900

    [fix] 이미지 관련 로직 삭제 (보안)

--- src/main/java/com/prgrms/rg/domain/common/file/application/exception/IllegalFileExtensionException.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.rg.domain.common.file.application.exception;
-
-public class
-IllegalFileExtensionException extends IllegalArgumentException {
-	public IllegalFileExtensionException(String extension) {
-		super("확장자 " + extension + " 은 지원하지 않습니다.");
-	}
-}
