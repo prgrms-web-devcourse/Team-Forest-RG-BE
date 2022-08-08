59fd3a18ca380e12418f1193be6d741374b7e2e8
Hanbin Lee <logos1018@gmail.com>
2022-08-08 17:27:25 +0900

    [feat] 중복 닉네임 시도 시 에러 처리 정의

--- src/main/java/com/prgrms/rg/web/user/api/UserRestControllerAdvice.java
+++ src/main/java/com/prgrms/rg/web/user/api/UserRestControllerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.prgrms.rg.domain.user.application.exception.DuplicateNicknameException;
 import com.prgrms.rg.domain.user.application.exception.NoSuchUserException;
 import com.prgrms.rg.web.common.RgControllerAdvice;
 
@@ -18,5 +19,12 @@ public class UserRestControllerAdvice {
 	public String handleNoSuchUserException(NoSuchUserException e) {
 		log.info(e.getMessage(), e);
 		return "유저 정보를 불러오는 데 실패했습니다. 적합한 id로 요청을 보내지 않았을 수 있으며, 요청을 보내는 사이에 해당 유저 정보가 삭제되었을 수 있습니다.";
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(DuplicateNicknameException.class)
+	public String handleDuplicateNicknameException(DuplicateNicknameException e) {
+		log.info(e.getMessage(), e);
+		return "다른 유저가 사용중인 닉네임을 사용할 수는 없습니다.";
 	}
 }
