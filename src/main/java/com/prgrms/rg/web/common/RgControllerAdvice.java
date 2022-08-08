9faf3241ca6de512c23597b5b6434662e0069cd7
Hanbin Lee <logos1018@gmail.com>
2022-08-08 16:57:27 +0900

    [fix] 커스텀 ControllerAdvice 수정

--- src/main/java/com/prgrms/rg/web/common/RgControllerAdvice.java
+++ src/main/java/com/prgrms/rg/web/common/RgControllerAdvice.java
@@ -3,13 +3,21 @@ package com.prgrms.rg.web.common;
 import static org.springframework.core.Ordered.*;
 
 import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ControllerAdvice
+@ResponseBody
 @Order(HIGHEST_PRECEDENCE)
 public @interface RgControllerAdvice {
 	@AliasFor(annotation = ControllerAdvice.class)
