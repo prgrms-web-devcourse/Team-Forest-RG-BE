5bcf589d048165dc0c2a9b59639af2a4398dcba6
Minsung Kim <epicblue@hanmail.net>
2022-08-10 01:38:24 +0900

    [refactor] 파일 정리

--- src/main/java/com/prgrms/rg/domain/ridingpost/model/RidingPostNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.rg.domain.ridingpost.model;
-
-public class RidingPostNotFoundException extends RuntimeException {
-	public RidingPostNotFoundException(Long id) {
-		super("cannot find RidingPost id: '" + id + "'");
-	}
-
-}
