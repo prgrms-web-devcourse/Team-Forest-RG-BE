39eab757c9a9578e8de5488341be91228c6915aa
Minsung Kim <epicblue@hanmail.net>
2022-08-09 23:32:48 +0900

    [fix] 테스트 실행 여부 결정 로직 변경

--- src/test/java/com/prgrms/rg/web/common/CustomErrorControllerTest.java
+++ src/test/java/com/prgrms/rg/web/common/CustomErrorControllerTest.java
@@ -1,17 +1,16 @@
 package com.prgrms.rg.web.common;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Assumptions.*;
 
 import java.util.Map;
 
 import org.assertj.core.api.ThrowableAssert;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.DisabledIf;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,20 +18,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 기본 에러 처리 컨트롤러를 테스트 하려면 MockMvc가 아닌 실제 ServletContainer를 올려야 합니다.
- * 로컬에서 걸리는 테스트 시간을 고려하여 linux 환경(Github Action)에서만 테스트가 수행되도록 설정했습니다.
+ * 로컬에서 걸리는 테스트 시간을 고려하여 운영 체제 환경이 윈도우나 맥이 아닐 경우에만 작동하도록 바꿨습니다!
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisabledIf("#{systemProperties['os.name'].toLowerCase().matches('^.*(windows|mac).*$') }")
 class CustomErrorControllerTest {
 
 	@LocalServerPort
 	int portNumber;
 	@Autowired
 	ObjectMapper json;
-
-	@BeforeAll
-	static void ignoreTestIfSystemIsNotLinuxEnvironment() {
-		assumeThat(System.getProperty("os.name")).containsIgnoringCase("linux");
-	}
 
 	@Test
 	@DisplayName("서버가 등록하지 않은 API 요청을 할 경우 404 메시지가 담긴 Json을 응답한다.")
