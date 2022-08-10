c7f065004bad9b5dbf151e5545f74072472ff1e6
Minsung Kim <epicblue@hanmail.net>
2022-08-10 20:49:10 +0900

    [feat] 댓글 작성 게시물, 사용자 관련 예외 처리

--- src/test/java/com/prgrms/rg/web/ridingpost/api/RidingPostCommentControllerTest.java
+++ src/test/java/com/prgrms/rg/web/ridingpost/api/RidingPostCommentControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.rg.domain.auth.jwt.JwtTokenProvider;
+import com.prgrms.rg.domain.common.RelatedEntityNotFoundException;
 import com.prgrms.rg.domain.ridingpost.application.RidingPostCommentService;
 import com.prgrms.rg.domain.ridingpost.application.command.RidingPostCommentCreateCommand;
 import com.prgrms.rg.testutil.ControllerTest;
@@ -63,6 +64,36 @@ class RidingPostCommentControllerTest {
 		);
 		then(ridingPostCommentService).should(times(1)).createComment(command);
 
+	}
+
+	@Test
+	@DisplayName("유효하지 않은 사용자 id 혹은 라이딩 게시물 id로 요청했을 경우 정보 실패 메시지가 담긴 응답을 보낸다.")
+	void send_error_message_when_requested_with_invalid_related_entity_id() throws Exception {
+		// Given
+		String content = "content";
+		Map<String, Object> bodyMap = Map.of("content", content);
+		var body = objectMapper.writeValueAsString(bodyMap);
+		var authorId = 1L;
+		var postId = 2L;
+		var token = tokenProvider.createToken("ROLE_USER", authorId);
+		var command = RidingPostCommentCreateCommand.of(authorId, postId, content);
+		given(ridingPostCommentService.createComment(command)).willThrow(RelatedEntityNotFoundException.class);
+
+		// When
+		var result = mockMvc.perform(
+			post("/api/v1/ridingposts/" + postId + "/comments")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(HttpHeaders.AUTHORIZATION, "token " + token)
+				.content(body)
+		);
+
+		// Then
+
+		result.andExpectAll(
+			status().isUnprocessableEntity(),
+			jsonPath("message").value("필요한 사용자 정보, 라이딩 정보를 불러오는 데 실패했습니다.")
+		);
+		then(ridingPostCommentService).should(times(1)).createComment(command);
 	}
 
 }
