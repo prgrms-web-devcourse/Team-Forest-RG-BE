3e38f2726ae05cf9d8cf9f7a4cc2d048e67586e9
kkj0419 <kkhi0419@gmail.com>
2022-08-10 12:12:08 +0900

    [test] ridingpost 서비스 수정 테스트

--- src/test/java/com/prgrms/rg/domain/ridingpost/application/impl/RidingPostServiceImplTest.java
+++ src/test/java/com/prgrms/rg/domain/ridingpost/application/impl/RidingPostServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,16 +14,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.prgrms.rg.domain.common.model.metadata.RidingLevel;
 import com.prgrms.rg.domain.ridingpost.application.command.RidingConditionSaveCommand;
 import com.prgrms.rg.domain.ridingpost.application.command.RidingSaveCommand;
 import com.prgrms.rg.domain.ridingpost.application.command.RidingMainSaveCommand;
 import com.prgrms.rg.domain.ridingpost.application.command.RidingParticipantSaveCommand;
+import com.prgrms.rg.domain.ridingpost.application.command.RidingSubSaveCommand;
 import com.prgrms.rg.domain.ridingpost.model.AddressCode;
+import com.prgrms.rg.domain.ridingpost.model.Coordinate;
+import com.prgrms.rg.domain.ridingpost.model.RidingPost;
 import com.prgrms.rg.domain.ridingpost.model.RidingPostRepository;
 import com.prgrms.rg.domain.user.model.Manner;
 import com.prgrms.rg.domain.user.model.Nickname;
 import com.prgrms.rg.domain.user.model.User;
 import com.prgrms.rg.domain.user.model.UserRepository;
+import com.prgrms.rg.testutil.TestEntityDataFactory;
 
 @SpringBootTest
 @Transactional
@@ -59,7 +65,7 @@ class RidingPostServiceImplTest {
 		var createCommand = new RidingSaveCommand(null,
 			mainCreateCommand,
 			new RidingParticipantSaveCommand(6, 10),
-			new RidingConditionSaveCommand("BEGINNER", List.of("MTB")), null
+			new RidingConditionSaveCommand(RidingLevel.BEGINNER.getLevelName(), List.of("MTB")), null
 		);
 
 	    //when
@@ -69,5 +75,45 @@ class RidingPostServiceImplTest {
 	    //then
 		assertThat(savedOne.isPresent(), is(true));
 		assertThat(savedOne.get().getRidingMainSection(), is(samePropertyValuesAs(mainCreateCommand.toSection())));
+	}
+
+	@Test
+	@Transactional
+	@DisplayName("ridingpost 수정 테스트")
+	@Sql(scripts = {"classpath:address_code.sql", "classpath:bicycle.sql"})
+	void updateRidingTest(){
+		//user-post
+		User leader = userRepository.save(TestEntityDataFactory.createUser());
+		RidingPost post = ridingPostRepository.save(TestEntityDataFactory.createRidingPost(leader.getId()));
+
+		var conditionCommand = new RidingConditionSaveCommand("상", List.of("MTB"));
+		var participantCommand = new RidingParticipantSaveCommand(post.getRidingParticipantSection().getMinParticipantCount(),
+			post.getRidingParticipantSection().getMaxParticipantCount());
+		var subCommand = new RidingSubSaveCommand("new-sub title", "new-sub content",
+			Collections.emptyList());
+
+		var mainCommand = RidingMainSaveCommand.builder()
+			.title("자전거가 타고싶어요")
+			.ridingDate(LocalDateTime.now().plusDays(10L))
+			.routes(List.of("중앙 공원", "능골 공원", "탑골 공원"))
+			.estimatedTime("3시간")
+			.addressCode(new AddressCode(11010))
+			.fee(10000)
+			.departurePlace(new Coordinate(35.232600, 127.650250)).build();
+
+		var updateCommand = new RidingSaveCommand(null, mainCommand, participantCommand, conditionCommand, List.of(subCommand));
+
+		//when
+		ridingPostService.updateRidingPost(leader.getId(), post.getId(), updateCommand);
+
+		var updatedPost = ridingPostRepository.findById(post.getId());
+
+		assertThat(updatedPost.isPresent(), is(true));
+		assertThat(updatedPost.get().getLeader().getId(), is(equalTo(leader.getId())));
+		assertThat(updatedPost.get().getRidingMainSection().getEstimatedTime(), is(equalTo(
+			mainCommand.getEstimatedTime())));
+		assertThat(updatedPost.get().getSubSectionList(), is(hasSize(1)));
+		assertThat(updatedPost.get().getSubSectionList().get(0).getTitle(), is(equalTo(subCommand.getTitle())));
+
 	}
 }
