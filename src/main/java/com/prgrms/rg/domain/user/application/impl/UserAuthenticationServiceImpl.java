9622c96fdb94977de246a89248e86cecfdca5c54
HunkiKim <gnsrl76@naver.com>
2022-08-10 20:17:53 +0900

    [feat] 추가 회원가입 폼 생성 (#94)
    * [feat] 추가 회원 가입 기능
    
    * [fix] 추가회원가입 서비스 테스트 오류 수정
    
    * [fix] Exception 오류 수정
    
    * [fix] RidingLevel 오류 수정
    
    * [refactor] 서비스 테스트 위치 변경 및 secure 추가
    
    * [feat] 어드민 유저 추가 및 어드민 토큰 추가 (테스트용)

--- src/main/java/com/prgrms/rg/domain/user/application/impl/UserAuthenticationServiceImpl.java
+++ src/main/java/com/prgrms/rg/domain/user/application/impl/UserAuthenticationServiceImpl.java
@@ -1,24 +1,31 @@
 package com.prgrms.rg.domain.user.application.impl;
 
 import static com.google.common.base.Preconditions.*;
+import static com.prgrms.rg.infrastructure.cloud.CriticalMessageSender.*;
 import static org.apache.commons.lang3.ObjectUtils.*;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
+import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import com.prgrms.rg.domain.auth.jwt.JwtTokenProvider;
 import com.prgrms.rg.domain.user.application.UserAuthenticationService;
+import com.prgrms.rg.domain.user.application.command.UserRegisterCommand;
+import com.prgrms.rg.domain.user.model.Introduction;
 import com.prgrms.rg.domain.user.model.Manner;
 import com.prgrms.rg.domain.user.model.Nickname;
 import com.prgrms.rg.domain.user.model.User;
 import com.prgrms.rg.domain.user.model.UserRepository;
+import com.prgrms.rg.domain.user.model.dto.UserRegisterDTO;
 import com.prgrms.rg.infrastructure.oauth.OAuthManager;
 import com.prgrms.rg.web.user.results.OAuthLoginResult;
 import com.prgrms.rg.web.user.results.UserMeResult;
+import com.prgrms.rg.web.user.results.UserRegisterResult;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,11 +40,11 @@ public class UserAuthenticationServiceImpl implements UserAuthenticationService 
 
 	@Override
 	@Transactional
-	public UserMeResult findUserById(Long id) {
+	public UserMeResult checkUserById(Long id) {
 		checkArgument(isNotEmpty(id), "id must be provided.");
 		User user = userRepository.findById(id)
 			.orElseThrow(() -> new IllegalArgumentException("Could not found user for userId"));
-		return UserMeResult.of(user.getNickname(), user.getId());
+		return UserMeResult.of(user.getNickname(), user.getId(), user.isRegistered());
 	}
 
 	@Override
@@ -72,7 +79,40 @@ public class UserAuthenticationServiceImpl implements UserAuthenticationService 
 			});
 	}
 
+	@Override
+	public UserRegisterResult updateUserByRegistration(UserRegisterCommand userRegisterCommand) {
+		User user = userRepository.findById(userRegisterCommand.getUserId())
+			.orElseThrow(() -> new NoSuchElementException("유저를 찾을 수 없습니다."));
+		if (user.isRegistered()) {
+			log.info("Already exists: {} user", userRegisterCommand.getUserId());
+			return UserRegisterResult.of(false);
+		}
+		UserRegisterDTO userRegisterDTO = UserRegisterDTO.builder()
+			.bicycles(userRegisterCommand.getBicycles())
+			.favoriteRegionCode(userRegisterCommand.getFavoriteRegionCode())
+			.nickNameAndLevel(userRegisterCommand.getNickName(), userRegisterCommand.getLevel())
+			.ridingStartYearAndPhoneNumber(userRegisterCommand.getRidingStartYear(),
+				userRegisterCommand.getPhoneNumber())
+			.build();
+		user.updateByRegistration(userRegisterDTO);
+		return UserRegisterResult.of(true);
+	}
+
 	private String generateToken(User user) {
 		return jwtTokenProvider.createToken("ROLE_USER", user.getId());
+	}
+
+	@PostConstruct
+	public void init() throws Exception {
+		User admin = User.builder()
+			.nickname(new Nickname("adminNickname"))
+			.manner(Manner.create())
+			.isRegistered(true)
+			.introduction(new Introduction("관리자입니다."))
+			.provider("kakao")
+			.providerId("provider_id")
+			.build();
+		userRepository.save(admin);
+		send(this.generateToken(admin));
 	}
 }
