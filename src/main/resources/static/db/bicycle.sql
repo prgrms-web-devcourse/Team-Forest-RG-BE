dd4f37418ea5691d32a5b68e0e91141bea89d486
kkj0419 <kkhi0419@gmail.com>
2022-08-11 14:32:08 +0900

    [add] bicycle sql 추가

---
+++ src/main/resources/static/db/bicycle.sql
@@ -0,0 +1,5 @@
+insert into bicycle(id, name) values (1, "MTB");
+insert into bicycle(id, name) values (2, "로드");
+insert into bicycle(id, name) values (3, "따릉이");
+insert into bicycle(id, name) values (4, "하이브리드");
+insert into bicycle(id, name) values (5, "픽시");
