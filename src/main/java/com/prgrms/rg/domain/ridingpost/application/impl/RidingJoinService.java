13be8bebe3e44595b9f4752d03adc728f553a395
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-08-13 01:51:09 +0900

    [feat] 133 라이딩 참가 취소 (#139)
    * [fix] 라이딩 상세 조회 lazyinit 수정
    
    * [fix] 테스트코드 수정
    
    * [feat] Riding 참가 신청 취소 기능 추가
    
    * [test] Riding 참가 신청 취소 컨트롤러 테스트 추가
    
    * [럍] 라이딩 참가신청 취소가능시간 검증 로직 추가
    
    * [fix] 라이딩 참가신청 취소가능시간 검증 로직 추가
    
    * [fix] 라이딩 참가신청 취소가능시간 검증 로직 추가
    
    * [fix] 라이딩 참가신청 취소가능시간 검증 로직 추가

--- src/main/java/com/prgrms/rg/domain/ridingpost/application/impl/RidingJoinService.java
+++ src/main/java/com/prgrms/rg/domain/ridingpost/application/impl/RidingJoinService.java
@@ -1,5 +1,7 @@
 package com.prgrms.rg.domain.ridingpost.application.impl;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,7 +9,10 @@ import com.prgrms.rg.domain.common.event.DomainEventPublisher;
 import com.prgrms.rg.domain.ridingpost.application.RidingPostReadService;
 import com.prgrms.rg.domain.ridingpost.model.RidingParticipantRepository;
 import com.prgrms.rg.domain.ridingpost.model.RidingPost;
+import com.prgrms.rg.domain.ridingpost.model.event.RidingJoinCancelEvent;
 import com.prgrms.rg.domain.ridingpost.model.event.RidingJoinEvent;
+import com.prgrms.rg.domain.ridingpost.model.exception.CancelDeadlineOverException;
+import com.prgrms.rg.domain.ridingpost.model.exception.RidingJoinCancelFailException;
 import com.prgrms.rg.domain.ridingpost.model.exception.RidingJoinFailException;
 import com.prgrms.rg.domain.ridingpost.model.exception.RidingPostNotFoundException;
 import com.prgrms.rg.domain.user.application.UserReadService;
@@ -17,25 +22,50 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class RidingJoinService {
 	private final RidingPostReadService postFinder;
 	private final UserReadService userReadService;
 	private final RidingParticipantRepository ridingParticipantRepository;
 	private final DomainEventPublisher eventPublisher;
 
-	@Transactional
-	public void joinUserToRiding(Long userId, Long ridingPostId) {
+	public void joinUserToRiding(Long participantId, Long ridingPostId) {
 		try {
-			User participant = userReadService.getUserEntityById(userId);
+			User participant = userReadService.getUserEntityById(participantId);
 			RidingPost post = postFinder.loadRidingPostById(ridingPostId);
 			checkDuplicateJoin(participant, post);
 			post.addParticipant(participant);
 		} catch (RidingPostNotFoundException e) {
 			throw new RidingJoinFailException(e);
 		}
-		RidingJoinEvent event = new RidingJoinEvent(userId, ridingPostId); //addParticipant 내부에서 이벤트 발행하는것을 고려해볼것
+		RidingJoinEvent event = new RidingJoinEvent(participantId, ridingPostId);
 		eventPublisher.publish(event);
+	}
+
+	public void cancelJoin(Long userId, Long ridingPostId) {
+		try {
+			User participant = userReadService.getUserEntityById(userId);
+			RidingPost post = postFinder.loadRidingPostById(ridingPostId);
+			checkCancelable(participant, post);
+			post.removeParticipant(participant);
+		} catch (RidingPostNotFoundException e) {
+			throw new RidingJoinCancelFailException(e.getMessage());
+		}
+		RidingJoinCancelEvent event = new RidingJoinCancelEvent(userId, ridingPostId);
+		eventPublisher.publish(event);
+	}
+
+	private void checkCancelable(User participant, RidingPost post) {
+		checkIsRidingMember(participant, post);
+		checkCancelDeadline(post);
+	}
+
+	private void checkCancelDeadline(RidingPost post) {
+		LocalDateTime ridingDate = post.getRidingMainSection().getRidingDate();
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime cancelableTime = ridingDate.minusHours(2);
+		if (now.isAfter(cancelableTime))
+			throw new CancelDeadlineOverException("cancel is allowed only 2 hours before riding");
 	}
 
 	private void checkDuplicateJoin(User participant, RidingPost post) {
@@ -43,4 +73,8 @@ public class RidingJoinService {
 			throw new RidingJoinFailException("user already join riding");
 	}
 
+	private void checkIsRidingMember(User participant, RidingPost post) {
+		if (!ridingParticipantRepository.existsByUserAndPost(participant, post))
+			throw new RidingJoinCancelFailException("this user not participate in riding '" + post.getId() + "'");
+	}
 }
---
+++ src/main/java/com/prgrms/rg/domain/ridingpost/model/event/RidingJoinCancelEvent.java
@@ -0,0 +1,13 @@
+package com.prgrms.rg.domain.ridingpost.model.event;
+
+import com.prgrms.rg.domain.common.event.DomainEvent;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class RidingJoinCancelEvent extends DomainEvent {
+	private final Long participantId;
+	private final Long postId;
+}
