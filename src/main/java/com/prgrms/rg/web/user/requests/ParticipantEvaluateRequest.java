f86c4d9af349f87f89c5e8ff0a6d1aa95f32950d
kkj0419 <kkhi0419@gmail.com>
2022-08-13 14:50:04 +0900

    [add] 생성자 추가

--- src/main/java/com/prgrms/rg/web/user/requests/ParticipantEvaluateRequest.java
+++ src/main/java/com/prgrms/rg/web/user/requests/ParticipantEvaluateRequest.java
@@ -2,14 +2,18 @@ package com.prgrms.rg.web.user.requests;
 
 import com.prgrms.rg.domain.user.application.command.ParticipantEvaluateCommand;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ParticipantEvaluateRequest {
 
-	private final Long memberId;
-	private final boolean recommended;
-	private final boolean noshow;
+	private Long memberId;
+	private boolean recommended;
+	private boolean noshow;
 
 	public ParticipantEvaluateCommand toCommand() {
 		return new ParticipantEvaluateCommand(memberId, recommended, noshow);
--- src/main/java/com/prgrms/rg/web/user/requests/RidingEvaluateRequest.java
+++ src/main/java/com/prgrms/rg/web/user/requests/RidingEvaluateRequest.java
@@ -2,9 +2,13 @@ package com.prgrms.rg.web.user.requests;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RidingEvaluateRequest {
 
 	private Long postId;
