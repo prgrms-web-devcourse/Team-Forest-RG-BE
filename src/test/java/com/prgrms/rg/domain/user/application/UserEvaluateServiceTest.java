72853e802e94b2bcf8d9ed46549f7fddf3a10e81
kkj0419 <kkhi0419@gmail.com>
2022-08-13 14:45:53 +0900

    [test] member evaluate 서비스 테스트 추가

--- src/test/java/com/prgrms/rg/domain/user/application/UserEvaluateServiceTest.java
+++ src/test/java/com/prgrms/rg/domain/user/application/UserEvaluateServiceTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.rg.domain.ridingpost.application.impl.RidingJoinService;
 import com.prgrms.rg.domain.ridingpost.model.RidingPostRepository;
-import com.prgrms.rg.domain.user.application.command.EvaluatedFromLeaderCommand;
+import com.prgrms.rg.domain.user.application.command.ParticipantEvaluateCommand;
+import com.prgrms.rg.domain.user.model.User;
 import com.prgrms.rg.domain.user.model.UserRepository;
 import com.prgrms.rg.testutil.TestEntityDataFactory;
 
@@ -40,30 +42,36 @@ class UserEvaluateServiceTest {
 	@Autowired
 	private EntityManager em;
 
+	User leader;
+	User member1;
+	User member2;
+	Long ridingpostId = 1L;
+
+	@BeforeEach
+	void setup() {
+		leader = userRepository.save(TestEntityDataFactory.createUser(1L, "leader"));
+		member1 = userRepository.save(TestEntityDataFactory.createUser(2L, "memberOne"));
+		member2 = userRepository.save(TestEntityDataFactory.createUser(3L, "memberTwo"));
+
+		joinService.joinUserToRiding(member1.getId(), ridingpostId);
+		joinService.joinUserToRiding(member2.getId(), ridingpostId);
+	}
+
 	//noshow, recommend count 반영 확인
 	@Test
 	@DisplayName("리더의 평가 테스트")
 	void evaluateFromLeaderTest() {
 
 		//given
-		var leader = userRepository.save(TestEntityDataFactory.createUser(1L, "leader"));
-		var member1 = userRepository.save(TestEntityDataFactory.createUser(2L, "memberOne"));
-		var member2 = userRepository.save(TestEntityDataFactory.createUser(3L , "memberTwo"));
-
-		var ridingpostId = 1L;
-
-		joinService.joinUserToRiding(member1.getId(), ridingpostId);
-		joinService.joinUserToRiding(member2.getId(), ridingpostId);
-
 		em.flush();
 		em.clear();
 
 		var commandList = List.of(
-			new EvaluatedFromLeaderCommand(member1.getId(), true, false),
-			new EvaluatedFromLeaderCommand(member2.getId(), false, false));
+			new ParticipantEvaluateCommand(member1.getId(), true, false),
+			new ParticipantEvaluateCommand(member2.getId(), false, false));
 
 		//when
-		userEvaluateService.evaluateFromLeader(leader.getId(), ridingpostId, commandList);
+		userEvaluateService.evaluateMembers(leader.getId(), ridingpostId, commandList);
 
 		em.flush();
 		em.clear();
@@ -73,6 +81,34 @@ class UserEvaluateServiceTest {
 		var participants = updatedRiding.get().getRidingParticipantSection().getParticipants();
 		assertThat(participants, is(hasSize(3)));
 		var findUser = userRepository.findById(participants.get(1).getUser().getId());
+		assertThat(findUser.isPresent(), is(true));
+		assertThat(findUser.get().getMannerInfo().getMannerPoint(), is(equalTo(1)));
+	}
+
+	@Test
+	@DisplayName("멤버의 평가 리스트")
+	void evaluateFromMemerTest() {
+		//given
+
+		em.flush();
+		em.clear();
+
+		var commandList = List.of(
+			new ParticipantEvaluateCommand(leader.getId(), true, false),
+			new ParticipantEvaluateCommand(member2.getId(), false, false));
+
+		//when
+		userEvaluateService.evaluateMembers(member1.getId(), ridingpostId, commandList);
+
+		em.flush();
+		em.clear();
+
+		//then
+		var updatedRiding = postRepository.findById(ridingpostId);
+		assertThat(updatedRiding.isPresent(), is(true));
+		var participants = updatedRiding.get().getRidingParticipantSection().getParticipants();
+		assertThat(participants, is(hasSize(3)));
+		var findUser = userRepository.findById(participants.get(0).getUser().getId());
 		assertThat(findUser.isPresent(), is(true));
 		assertThat(findUser.get().getMannerInfo().getMannerPoint(), is(equalTo(1)));
 	}
