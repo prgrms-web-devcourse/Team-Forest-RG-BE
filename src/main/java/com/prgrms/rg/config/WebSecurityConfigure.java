d9d7d43e103487e07e930de9f07cd273d0f5d510
Minsung Kim <epicblue@hanmail.net>
2022-08-15 02:01:46 +0900

    [setting] cors 설정 포트 와일드카드 추가 3

--- src/main/java/com/prgrms/rg/config/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/rg/config/WebSecurityConfigure.java
@@ -57,7 +57,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOriginPatterns(List.of("https://cool-dusk-ced14a.netlify.app","http://211.201.93.105:[*]","http://124.50.252.111:[*]","http://211.226.115.222:[*]","http://localhost:[*]"));
+		configuration.setAllowedOriginPatterns(List.of("https://cool-dusk-ced14a.netlify.app","http://localhost:[*]", "http://127.0.0.1:[*]"));
 		configuration.addAllowedHeader("*");
 		configuration.addAllowedMethod("*");
 		configuration.setAllowCredentials(true);
