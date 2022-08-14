c943cfbfdb6c2668ba15ac3e274e93eb1ad5e7ab
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-08-15 04:46:34 +0900

    [feat] 알림 삭제 api 추가

--- src/main/java/com/prgrms/rg/web/notification/api/NotificationController.java
+++ src/main/java/com/prgrms/rg/web/notification/api/NotificationController.java
@@ -3,7 +3,10 @@ package com.prgrms.rg.web.notification.api;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 public class NotificationController {
 	private final NotificationService notificationService;
 
+	@Secured("ROLE_USER")
 	@GetMapping("/api/notifications")
 	public Page<NotificationInfo> getNotifications(
 		@PageableDefault(sort = "createdAt") Pageable pageable,
@@ -27,4 +31,11 @@ public class NotificationController {
 		return notificationService.loadPagedNotificationByUser(auth.userId, pageable);
 	}
 
+	@Secured("ROLE_USER")
+	@DeleteMapping("/api/notifications")
+	public ResponseEntity<String> deleteNotifications(
+		@AuthenticationPrincipal JwtAuthentication auth) {
+		notificationService.deleteAllNotificationByUser(auth.userId);
+		return ResponseEntity.ok("notification deleted");
+	}
 }
